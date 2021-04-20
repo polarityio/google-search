52a7104f2ecaff857fb5dfe6038d0d27957a6206
radar989 <joem@polarity.io>
2021-04-20 11:43:22 -0400

    Initial development of the google search integration. The Polarity Google Search integration allows the analyst to retrieve and display search results from Google, just like you were to search for terms using google.com.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Polarity - Google Search Integration
+
+The Polarity Google Search integration allows the analyst to retrieve and display search results from Google, just like you were to search for terms using google.com. 
+
+
+## Google Search Options
+
+### API Key
+
+Valid Google CSE API Key
+
+### Max Results
+
+Maximum Number of results to return per query.
+
+## Installation Instructions
+
+Generate a new CSE API key on the following page:
+
+https://developers.google.com/custom-search/v1/overview
+
+A free account allows for up to 100 search queries per day.
+
+Once the API key has been generated, navigate to the CSE configuration site to create a new service engine:
+
+https://cse.google.com/cse/create/new
+
+Once the search engine has been generated, you can edit it to copy the Search engine ID which you will need for the integration settings.  Add additional sites that you would like to search on the edit page as well.
+
+For a more detailed video tutorial, [Look Here](https://www.youtube.com/watch?v=vP_inGfKG5E).
+
+Additional installation instructions for integrations are provided on the [PolarityIO GitHub Page](https://polarityio.github.io/).
+
+## Polarity
+
+Polarity is a memory-augmentation platform that improves and accelerates analyst decision making.  For more information about the Polarity platform please see:
+
+https://polarity.io/
---
+++ config/config.js
@@ -0,0 +1,118 @@
+module.exports = {
+  /**
+   * Name of the integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @required
+   */
+  name: 'Google',
+  /**
+   * The acronym that appears in the notification window when information from this integration
+   * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
+   * for the integration.  As a result, it is best to keep it to 4 or less characters.  The casing used
+   * here will be carried forward into the notification window.
+   *
+   * @type String
+   * @required
+   */
+  acronym: 'G',
+  /**
+   * Description for this integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @optional
+   */
+  description:
+    "Google integration allows a Polarity user to retrieve and display search results from Google programmatically. Just like searching Google directly.",
+  entityTypes: ['hash', 'cve'],
+  customTypes:[
+    {
+      key: 'g:Search',
+      regex: /^g:.{1,256}/
+    },
+    {
+      key: 'all text',
+      regex: /^.{2,256}/
+    },
+    {
+      key: '@handle',
+      regex: /@(\w){1,24}/
+    }
+  ],
+  defaultColor: 'light-gray',
+  /**
+   * Provide custom component logic and template for rendering the integration details block.  If you do not
+   * provide a custom template and/or component then the integration will display data as a table of key value
+   * pairs.
+   *
+   * @type Object
+   * @optional
+   */
+  styles: ['./styles/style.less'],
+  block: {
+    component: {
+      file: './components/block.js'
+    },
+    template: {
+      file: './templates/block.hbs'
+    }
+  },
+  summary: {
+    component: {
+      file: './components/summary.js'
+    },
+    template: {
+      file: './templates/summary.hbs'
+    }
+  },
+  request: {
+    // Provide the path to your certFile. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    cert: '',
+    // Provide the path to your private key. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    key: '',
+    // Provide the key passphrase if required.  Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    passphrase: '',
+    // Provide the Certificate Authority. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    ca: '',
+    // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
+    // the url parameter (by embedding the auth info in the uri)
+    proxy: '',
+
+    rejectUnauthorized: true
+  },
+  logging: {
+    level: 'info' //trace, debug, info, warn, error, fatal
+  },
+  onDemandOnly: true,
+  /**
+   * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
+   * as an array of option objects.
+   *
+   * @type Array
+   * @optional
+   */
+  options: [
+    {
+      key: 'apiKey',
+      name: 'API Key',
+      description: 'Valid Google CSE API Key',
+      default: '',
+      type: 'password',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'maxResults',
+      name: 'Maximum Number of Results to Return',
+      description: 'Maximum number of search results to return',
+      default: 10,
+      type: 'number',
+      userCanEdit: false,
+      adminOnly: true
+    }
+  ]
+};
