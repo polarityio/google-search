84dabaceff292ffa282751b9cbc995f478e4679e
radar989 <joem@polarity.io>
2021-05-05 11:22:22 -0400

    updated regexes

--- config/config.js
+++ config/config.js
@@ -28,11 +28,11 @@ module.exports = {
   customTypes:[
     {
       key: 'g:Search',
-      regex: /^g:.{1,256}/
+      regex: /^g:.{2,2048}/
     },
     {
       key: 'all text',
-      regex: /^.{2,256}/
+      regex: /^[\s\S]{2,2048}$/
     },
     {
       key: '@handle',
