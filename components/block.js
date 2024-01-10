5e5be99588e24ef7c0862a5154e8a66ed6006224
Nick Smith <nicktsmith7@gmail.com>
2024-01-10 03:11:26 -0800

    feat: add search disclaimer

--- components/block.js
+++ components/block.js
@@ -1,3 +1,43 @@
 polarity.export = PolarityComponent.extend({
-  details: Ember.computed.alias('block.data.details')
+  details: Ember.computed.alias('block.data.details'),
+  actions: {
+    acceptDisclaimer: function () {
+      this.set('details.showDisclaimer', false);
+      this.set('details.acceptedDisclaimer', true);
+      this.search();
+    },
+    declineDisclaimer: function () {
+      const payload = {
+        action: 'declineDisclaimer',
+        search: this.get('details.response.choices')
+      };
+
+      this.sendIntegrationMessage(payload)
+        .then((result) => {
+          this.set('details.showDisclaimer', false);
+          this.set('details.disclaimerDeclined', result.declined);
+        })
+        .catch((error) => {
+          consol;
+          this.set('errorMessage', JSON.stringify(error, null, 2));
+        });
+    }
+  },
+  search: function () {
+    const payload = {
+      action: 'search',
+      entity: this.get('block.entity')
+    };
+
+    this.sendIntegrationMessage(payload)
+      .then((result) => {
+        this.set('details.items', result.data.details.items);
+        this.set('details.showDisclaimer', false);
+        this.set('details.disclaimerDeclined', false);
+      })
+      .catch((error) => {
+        console.log(error);
+        this.set('errorMessage', JSON.stringify(error, null, 2));
+      });
+  }
 });
